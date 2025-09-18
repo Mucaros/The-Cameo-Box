940973af40b9acd31f92f0a2640d59655b34389d
Mucaro <sillymucaro@gmail.com>
2025-09-18 18:56:20 +0200

    Update settings.py

--- Backend/cameo_backend/settings.py
+++ Backend/cameo_backend/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nsi0$wxlk3i!so@=5q%&#@2u_+n%66ky!m2yj*502rdl*dl#al'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 
 
 ALLOWED_HOSTS = []
 
